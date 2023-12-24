4351262bff83e1bbb9f7672a6db2c5f59326d5c3
Ayyaz yousaf <yousafayyaz882@gmail.com>
2023-12-24 10:58:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import "./App.css";
 
 function App() {
+  const [count, Setcount] = useState(0);
+  const [item, Setitem] = useState(0);
+
+  
+  const Miltememo = useMemo( function milticount(){
+
+    console.log("milticount")
+  },[count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> UseMemo Hook in React js </h1>
+      <h2> Count : {count} </h2>
+      <h2> Item: {item} </h2>
+       <h2> {Miltememo} </h2>
+      <button onClick={() => Setcount(count + 1)}> Update Count </button>
+      <button onClick={() => Setitem(item + 5)}> Update Count </button>
     </div>
   );
 }
